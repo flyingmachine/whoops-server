87f54bf13fd4c788dd5d8dea3433bde5b266439a
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2011-05-13 08:21:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whoops::Application.config.secret_token = '77bd233485c3fa89f62825b4bc7d87cfe271400b8bd5a9841aa784a5de81222e0f049985a5fe6caf0f272b699095764229bbfe4d2ae0da2cadaf86786ba113b9'
